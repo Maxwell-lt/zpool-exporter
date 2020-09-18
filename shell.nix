33822edc83eb6fcce2d48a3131057184faab9c16
Maxwell L-T <maxwell.lt@live.com>
2020-09-18 00:48:28 -0400

    Upgrade rust tooling

--- shell.nix
+++ shell.nix
@@ -1,13 +1,22 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustNightlyChannel = (nixpkgs.rustChannelOf { date = "2020-09-18"; channel = "nightly"; }).rust.override {
+    extensions = [
+      "rust-src"
+      "rls-preview"
+      "clippy-preview"
+      "rustfmt-preview"
+    ];
+  };
 in
 with nixpkgs;
 stdenv.mkDerivation {
   name = "moz_overlay_shell";
   buildInputs = [
-    # to use the latest nightly:
-    nixpkgs.latest.rustChannels.nightly.rust
+    rustNightlyChannel
+    rls
+    rustup
    ];
   RUST_BACKTRACE = 1;
 }
